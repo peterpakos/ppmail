b78cace3e9fc2a73942d951bd5a203fb7786d515
Peter Pakos <peter.pakos@wandisco.com>
2016-08-03 11:39:21 +0100

    Fix apikey handling, add cc option

--- WDMail.py
+++ WDMail.py
@@ -1,19 +1,25 @@
+# WANdisco Mail module
 #
-# A class to send mail via sendgrid
+# Version 16.8.3
 #
 # Author: Peter Pakos <peter.pakos@wandisco.com>
 
 from __future__ import print_function
 import sendgrid
+import urllib2
+import os
 
 
 class WDMail(object):
     def __init__(self, api_key):
-        self._sg = sendgrid.SendGridAPIClient(apikey=api_key)
+        os.environ['SENDGRID_API_KEY'] = api_key
+        self._sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
 
     def send(self, sender, recipients, subject, message, html=False, cc=None):
         content = ''
+        content_type = 'text/plain'
         if html:
+            content_type = 'text/html'
             content += '''
 <html>
 <body>
@@ -31,30 +37,32 @@ class WDMail(object):
         for recipient in recipients:
             recipient_list.append({"email": recipient})
         data = {
-            "personalizations": [
+            'personalizations': [
                 {
-                    "to": recipient_list,
-                    "subject": subject
+                    'to': recipient_list,
+                    'subject': subject
                 }
             ],
-            "from": {
-                "email": sender
+            'from': {
+                'email': sender
             },
-            "content": [
+            'content': [
                 {
-                    "type": "text/plain",
-                    "value": content
+                    'type': content_type,
+                    'value': content
                 }
             ]
         }
-#        if cc:
-#            mail.add_cc(cc)
-#        if html:
-#            mail.set_html(body)
-#        else:
-#            mail.set_text(body)
-        response = self._sg.client.mail.send.post(request_body=data)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+        if cc is not None:
+            cc_list = []
+            for c in cc:
+                cc_list.append({'email': c})
+            data['personalizations'][0]['cc'] = cc_list
+        response = None
+        try:
+            response = self._sg.client.mail.send.post(request_body=data)
+        except urllib2.HTTPError as err:
+            print(err)
+            exit(1)
+
         return response.status_code
